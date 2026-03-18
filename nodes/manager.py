9b448addb2e2760fba2c4c700b6762dac633466f
mdsaibhossain <saibhossain5@gmail.com>
2026-03-18 23:19:00 +0600

    node add

---
+++ nodes/manager.py
@@ -0,0 +1,7 @@
+
+
+def manager_node(state: InterviewState):
+    plan = state.get("interview_plan", [])
+    if not plan:
+        return {"current_topic": None}
+    return {"current_topic": plan[0], "interview_plan": plan[1:], "follow_up_count": 0}
