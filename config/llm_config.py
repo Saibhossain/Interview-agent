2392fd2215f8819281e0a2a390a3d018a55d8782
mdsaibhossain <saibhossain5@gmail.com>
2026-03-19 13:53:22 +0600

    config

--- config/llm_config.py
+++ config/llm_config.py
@@ -1,6 +1,18 @@
 from langchain.chat_models import init_chat_model
+from langchain_ollama import ChatOllama
 from config.settings import DEFAULT_MODEL
+from config.settings import OLLAMA_MODEL
+
+
+# def get_llm():
+#     # This ensures the LLM is only initialized after the user provides the API key in the UI
+#     return init_chat_model(DEFAULT_MODEL)
+
 
 def get_llm():
-    # This ensures the LLM is only initialized after the user provides the API key in the UI
-    return init_chat_model(DEFAULT_MODEL)
+    "use locally ollama model gemma3:1b, deepseek-r1:1.5b"
+
+    return ChatOllama(
+        model=OLLAMA_MODEL,
+        temperature=0.2,
+    )
